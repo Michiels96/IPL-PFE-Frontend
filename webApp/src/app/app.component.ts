90e60f737721305ba5594f18807ca0febd5c9c4d
Pierre Michiels <pierre.michiels@student.vinci.be>
2019-12-03 16:48:56 +0100

    push à la David :p

--- webApp/src/app/app.component.ts
+++ webApp/src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ApiService]
 })
 export class AppComponent {
-  title = 'angular-pwa';
+  enfants = [];
+  handicap = [];
+
+  constructor(private api: ApiService){
+    this.getEnfants();
+  }
+  getEnfants = () => {
+    this.api.getAllHandicaps().subscribe(
+      data => {
+        console.log(data);
+        this.handicap = data;
+      },
+      error => {
+        console.log(error);
+      }
+    )
+    this.api.getAllEnfants().subscribe(
+      data => {
+        console.log(data);
+        this.enfants = data;
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  }
 }
