24b41e0ceb81a1141abc30be88fe4dbc494ccc3d
davidS <david.sabo@hotmail.be>
2019-12-04 16:51:36 +0100

    commitbordel

---
+++ webApp/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import {environment} from '../environments/environment'
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js',{ enabled: environment.production})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
