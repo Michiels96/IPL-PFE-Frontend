0de90afaa42ffbd756698a8ff385fa10410ac9b4
davidS <david.sabo@hotmail.be>
2019-12-05 11:41:30 +0100

    components

---
+++ src/app/components/connection-form/connection-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-connection-form',
+  templateUrl: './connection-form.component.html',
+  styleUrls: ['./connection-form.component.css']
+})
+export class ConnectionFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
