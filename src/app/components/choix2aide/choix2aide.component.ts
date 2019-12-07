c65e3b2ba66ca05105e09efad94cabe0c8ea4d92
Pierre Michiels <pierre.michiels@student.vinci.be>
2019-12-07 18:10:36 +0100

    rename choix2&3

---
+++ src/app/components/choix2aide/choix2aide.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-choix2aide',
+  templateUrl: './choix2aide.component.html',
+  styleUrls: ['./choix2aide.component.css']
+})
+export class Choix2aideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
