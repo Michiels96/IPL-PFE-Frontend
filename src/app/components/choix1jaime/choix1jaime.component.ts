78a3640e48eb1dda903dbc031e0ed6c17c61e219
Pierre Michiels <pierre.michiels@student.vinci.be>
2019-12-07 16:57:59 +0100

    upgrade composant choix1jaime (1)

--- src/app/components/choix1jaime/choix1jaime.component.ts
+++ src/app/components/choix1jaime/choix1jaime.component.ts
@@ -9,17 +9,29 @@ import { Router } from '@angular/router';
 })
 export class Choix1jaimeComponent implements OnInit {
   var_imagesByLibelle = [];
-  constructor(private api: ApiService,private router:Router) { }
+  var_imagesAimees = [];
+  var_imagesPasAimees = [];
+  var_imagesEnregistrees = [this.var_imagesAimees, this.var_imagesPasAimees];
+  var_activiteCourante;
+  constructor(private api: ApiService,private router:Router) {}
+  
+  ngOnInit() {
+    this.getAllImagesByLibelle();
+  }
+
 
   getAllImagesByLibelle = () => {
     this.api.getAllImages().subscribe(
       data => {
-        console.log("BEFORE TRI "+data);
+        var i = 1;
         for(var key in data){
-          //console.log(data[key].categorie_image);
           if(data[key].categorie_image == "deplacements"){
             data[key].description = data[key].description+".jpg";
             this.var_imagesByLibelle.push(data[key]); 
+            if(i == 1){
+              this.var_activiteCourante = data[key];
+            }
+            i++;
           }
         }
         console.log("AFTER TRI "+JSON.stringify(this.var_imagesByLibelle));
@@ -30,8 +42,34 @@ export class Choix1jaimeComponent implements OnInit {
     )
   }
 
-  ngOnInit() {
-    this.getAllImagesByLibelle();
+  addImgToYes(activite){
+    if(this.var_activiteCourante.image_id < this.var_imagesByLibelle.length){
+      this.var_imagesEnregistrees[this.var_imagesAimees.push(activite)];
+      this.var_activiteCourante = this.var_imagesByLibelle[this.var_activiteCourante.image_id];  
+    }
+    else{
+      console.log("trop loin oui")
+    }
+    
+  }
+
+  addImgToNo(activite){
+    if(this.var_activiteCourante.image_id < this.var_imagesByLibelle.length){
+      this.var_imagesEnregistrees[this.var_imagesPasAimees.push(activite)];
+      this.var_activiteCourante = this.var_imagesByLibelle[this.var_activiteCourante.image_id];  
+    }
+    else{
+      console.log("trop loin non")
+    }
+  }
+
+  imgBypass(){
+    if(this.var_activiteCourante.image_id < this.var_imagesByLibelle.length){
+      this.var_activiteCourante = this.var_imagesByLibelle[this.var_activiteCourante.image_id];  
+    }
+    else{
+      console.log("trop loin bypass")
+    }
   }
 
 }
