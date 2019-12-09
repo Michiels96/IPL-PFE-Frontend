35f70ddc18a668b6717db56d4879bf48e66952db
davidS <david.sabo@hotmail.be>
2019-12-09 17:41:53 +0100

    connection_lost_fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { Component ,Input, Output} from '@angular/core';
+import { Component , OnInit, HostListener} from '@angular/core';
 import { ApiService } from './api.service';
-import { FormGroup } from '@angular/forms';
-import { tokenName } from '@angular/compiler';
+
 
 @Component({
   selector: 'app-root',
@@ -10,132 +9,34 @@ import { tokenName } from '@angular/compiler';
   providers: [ApiService]
 })
 export class AppComponent {
-  inscriptionForm: FormGroup;
-  enfants = [];
-  handicaps = [];
-  kid_selected;
-  //enfant_id;
-  @Input() isTokenValid=false;
-  token;
-  nom;
-  prenom;
-  age;
-  handicap;
-  categories;
-  isShowedForm=true;
-  isShowedChoixCat=true;
-
+ 
+kid_in_session;
   constructor(private api: ApiService){
     //this.getEnfants();
-   this.kid_selected={age:-1,enfant_id:-1,handicap:-1,handicaps:'',nom:'',prenom:''};
+   
   }
-  
-  goToChoix(){
-   this.isShowedChoixCat=false;
-   this.getCategories();
+  ngOnInit() {
+    this.sendDataBeforePageLeave();
   }
-  goToConnectForm(){
-    this.isShowedForm=false;
-  }
-
-  getCategories = () => {
-    this.api.getAllCategories().subscribe(
+  deconnecterEnfant(kid){
+    this.api.updateKid(kid,false).subscribe(
       data => {
         console.log(data);
-        this.categories = data;
       },
       error => {
         console.log(error);
       }
     )
   }
-
-  getAllImages = () => {
-    this.api.getAllImages().subscribe(
-      data => {
-        //console.log(data);
-      },
-      error => {
-        console.log(error);
-      }
-    )
-  }
-
- /* getEnfants = () => {
-    this.api.getAllHandicaps().subscribe(
-      data => {
-        console.log(data);
-        this.handicap = data;
-      },
-      error => {
-        console.log(error);
-      }
-    )
-    this.api.getAllEnfants().subscribe(
-      data => {
-        console.log(data);
-        this.enfants = data;
-      },
-      error => {
-        console.log(error);
-      }
-    )
-  }*/
-  kidClicked=(enfant) =>{
-    console.log(enfant.nom)
-    this.api.getOneKid(enfant.enfant_id).subscribe( 
-      data => {
-        console.log(data);
-        /*this.prenom = data.prenom;
-        this.nom = data.nom;
-        this.age = data.age;
-        this.handicap = data.handicaps;*/
-       this.kid_selected=data;
-      },
-      error => {
-        console.log(error);
-      }
-    )
-  }
-  /*updateKid = () => {
-    this.api.updateKid(this.kid_selected).subscribe( 
-      data => {
-        console.log(data);
-        /*this.prenom = data.prenom;
-        this.nom = data.nom;
-        this.age = data.age;
-        this.handicap = data.handicaps;
-       this.kid_selected=data;
-      },
-      error => {
-        console.log(error);
-      }
-    )
-  }*/
-  createKid = () => {
-    this.api.postKid(this.kid_selected).subscribe( 
-      data => {
-        console.log(data);
+   
+  @HostListener('window:beforeunload', ['$event'])
+  sendDataBeforePageLeave() {
+    if (sessionStorage.length > 0) {
+      if(sessionStorage.getItem('kid_connected')!=''){
+        this.deconnecterEnfant( (JSON.parse(sessionStorage.getItem('kid_connected'))));
         
-       this.enfants.push(data);
-      },
-      error => {
-        console.log(error);
       }
-    )
-  }
- 
-  deleteKid = () => {
-    this.api.delKid(this.kid_selected.enfant_id).subscribe( 
-      data => {
-        console.log(data);
-        
-      //this.getEnfants();
-      },
-      error => {
-        console.log(error);
-      }
-    )
-  }
+    } 
     
+  }
 }
