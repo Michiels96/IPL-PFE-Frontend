f8440ad178807909c008b71eadfb258de0a24f09
Pierre Michiels <pierre.michiels@student.vinci.be>
2019-12-09 15:16:26 +0100

    creation nv composant: syntheseDesChoix

---
+++ src/app/components/synthese-des-choix/synthese-des-choix.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-synthese-des-choix',
+  templateUrl: './synthese-des-choix.component.html',
+  styleUrls: ['./synthese-des-choix.component.css']
+})
+export class SyntheseDesChoixComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
