ffab7cebf1862c79ee275c1333757d716faef851
davidS <david.sabo@hotmail.be>
2019-12-11 22:40:08 +0100

    s

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -79,7 +79,7 @@ export class ApiService {
     return this.http.post(this.baseurl + '/enfant/enfants/', postContent, {headers:this.httpHeaders});
   }
   postInfosKid(kid,id): Observable<any>{
-    let postContent={id:id,date_naissance:kid.date_naissance,langue:kid.langue,dominance:kid.dominance,scolarite:kid.scolarite,type:kid.type, niveau:kid.niveau,besoin_particulier:kid.besoin_particulier, autre_besoin_particulier:kid.autre_besoin_particulier}//JSON.stringify(kid);
+    let postContent={info_supp_id:-1,enfant:id,date_naissance:kid.date_naissance,langue:kid.langue,dominance:kid.dominance,scolarite:kid.scolarite,type_enseignement:kid.type, niveau_scolaire:kid.niveau,besoin_particulier:kid.besoin_particulier, autre_besoin_particulier:kid.autre_besoin_particulier}//JSON.stringify(kid);
     return this.http.post(this.baseurl + '/enfant/info_supplementaire/', postContent, {headers:this.httpHeaders});
   }
   //date_naissance:kid.date_naissance,langue:kid.langue,dominance:kid.dominance,scolarite:kid.scolarite,type:kid.type, niveau:kid.niveau,besoin_particulier:kid.besoin_particulier, autre_besoin_particulier:kid.autre_besoin_particulier
