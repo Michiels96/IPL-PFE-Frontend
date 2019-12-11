cdba6e552332aa37b03f9e314c108f99d97ce38f
davidS <david.sabo@hotmail.be>
2019-12-11 20:27:59 +0100

    credentials_true

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -2,21 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpBackend, HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable} from 'rxjs';
 
-/*export const InterceptorSkipHeader = 'X-Skip-Interceptor';
 
-@Injectable()
-export class SkippableInterceptor implements HttpInterceptor {
+Injectable()
+export class HttpRequestInterceptor implements HttpInterceptor {
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (req.headers.has(InterceptorSkipHeader)) {
-      const headers = req.headers.delete(InterceptorSkipHeader);
-      return next.handle(req.clone({ headers }));
-    }
-
+  intercept(req: HttpRequest<any>, next: HttpHandler):
+    Observable<HttpEvent<any>> {
     
+      // console.log("interceptor: " + req.url);
+      req = req.clone({
+        withCredentials: true
+      });
+      
+      return next.handle(req);
   }
-
-}*/
+}
 @Injectable({
   providedIn: 'root'
 })
@@ -32,92 +32,92 @@ export class ApiService {
  /*,'Access-Control-Allow-Credentials': 'true',
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
-'Access-Control-Allow-Headers': '*','withCredentials': 'true' */});
+'Access-Control-Allow-Headers': '*',/'withCredentials': 'true' */});
   private http: HttpClient;
   constructor(/*private http: HttpClient*/ handler: HttpBackend) {
     this.http = new HttpClient(handler);
    }
   
   getAllCategories(): Observable<any>{
-    return this.http.get(this.baseurl + '/images/categories/', {headers:this.httpHeaders});
+    return this.http.get(this.baseurl + '/images/categories/', {headers:this.httpHeaders,withCredentials: true});
   }
 
   getAllImages(): Observable<any>{
-    return this.http.get(this.baseurl + '/images/images/', {headers:this.httpHeaders});
+    return this.http.get(this.baseurl + '/images/images/', {headers:this.httpHeaders,withCredentials: true});
   }
 
   getAllImagesByLibelle(libelle): Observable<any>{
-    return this.http.get(this.baseurl + '/images/'+libelle+'/', {headers:this.httpHeaders});
+    return this.http.get(this.baseurl + '/images/'+libelle+'/', {headers:this.httpHeaders,withCredentials: true});
   }
   getAllSessions(): Observable<any>{
-    return this.http.get(this.baseurl + '/sessions/full_sessions/', {headers:this.httpHeaders});
+    return this.http.get(this.baseurl + '/sessions/full_sessions/', {headers:this.httpHeaders,withCredentials: true});
   }
   getSessionsById(id): Observable<any>{
-    return this.http.get(this.baseurl + '/sessions/enfant_full_sessions/'+ id +'/', {headers:this.httpHeaders});
+    return this.http.get(this.baseurl + '/sessions/enfant_full_sessions/'+ id +'/', {headers:this.httpHeaders,withCredentials: true});
   }
   getAllEnfants(): Observable<any>{
-    return this.http.get(this.baseurl + '/enfant/enfants/', {headers:this.httpHeaders});
+    return this.http.get(this.baseurl + '/enfant/enfants/', {headers:this.httpHeaders,withCredentials: true});
   }
   getLoggedEnfants(): Observable<any>{
-    return this.http.get(this.baseurl + '/enfant/logged_enfant/', {headers:this.httpHeaders});
+    return this.http.get(this.baseurl + '/enfant/logged_enfant/', {headers:this.httpHeaders,withCredentials: true});
   }
   getUnloggedEnfants(): Observable<any>{
-    return this.http.get(this.baseurl + '/enfant/non_logged_enfant/', {headers:this.httpHeaders});
+    return this.http.get(this.baseurl + '/enfant/non_logged_enfant/', {headers:this.httpHeaders,withCredentials: true});
   }
   getAllHandicaps(): Observable<any>{
-    return this.http.get(this.baseurl + '/handicaps/', {headers:this.httpHeaders});
+    return this.http.get(this.baseurl + '/handicaps/', {headers:this.httpHeaders,withCredentials: true});
   }
   getOneKid(id): Observable<any>{
-    return this.http.get(this.baseurl + '/enfant/enfants/'+ id +'/', {headers:this.httpHeaders});
+    return this.http.get(this.baseurl + '/enfant/enfants/'+ id +'/', {headers:this.httpHeaders,withCredentials: true});
   }
   updateKid(kid,bool): Observable<any>{
     let updateContent={age:kid.age,enfant_id:kid.enfant_id,nom:kid.nom,prenom:kid.prenom,connecte:bool};
-    return this.http.put(this.baseurl + '/enfant/enfants/'+ kid.enfant_id +'/',updateContent, {headers:this.httpHeaders});
+    return this.http.put(this.baseurl + '/enfant/enfants/'+ kid.enfant_id +'/',updateContent, {headers:this.httpHeaders,withCredentials: true});
   }
   postKid(kid): Observable<any>{
     let postContent={id:kid.id,nom:kid.nom,prenom:kid.prenom,date_naissance:kid.date_naissance,age:kid.age,connecte:false,langue:kid.langue,dominance:kid.dominance,scolarite:kid.scolarite,type:kid.type, niveau:kid.niveau,besoin_particulier:kid.besoin_particulier, autre_besoin_particulier:kid.autre_besoin_particulier}//JSON.stringify(kid);
-    return this.http.post(this.baseurl + '/enfant/enfants/', postContent, {headers:this.httpHeaders});
+    return this.http.post(this.baseurl + '/enfant/enfants/', postContent, {headers:this.httpHeaders,withCredentials: true});
   }
   createSession(session): Observable<any>{
     let postContent = JSON.stringify(session);
-    return this.http.post(this.baseurl + '/sessions/sessions/',postContent, {headers:this.httpHeaders});
+    return this.http.post(this.baseurl + '/sessions/sessions/',postContent, {headers:this.httpHeaders,withCredentials: true});
   }
   createQuestion(question): Observable<any>{
     let postContent = JSON.stringify(question);
-    return this.http.post(this.baseurl + '/sessions/questions/',postContent, {headers:this.httpHeaders});
+    return this.http.post(this.baseurl + '/sessions/questions/',postContent, {headers:this.httpHeaders,withCredentials: true});
   }
 
   getUser(id): Observable<any>{
-    return this.http.get(this.baseurl + '/prof/users/' + id +'/', {headers:this.httpHeaders});
+    return this.http.get(this.baseurl + '/prof/users/' + id +'/', {headers:this.httpHeaders,withCredentials: true});
   }
   postUser(user): Observable<any>{
     let postContent={id: user.id,username:user.email,password:user.password,email:user.email}//JSON.stringify(user);
-    return this.http.post(this.baseurl + '/prof/users/',postContent, {headers:this.httpHeaders});
+    return this.http.post(this.baseurl + '/prof/users/',postContent, {headers:this.httpHeaders,withCredentials: true});
   }
   postProf(prof,id_user): Observable<any>{
     let postContent={nom:prof.nom,prenom:prof.prenom,profession:prof.profession,autre_profession:prof.autre,telephone:prof.telephone,user:id_user};
-    return this.http.post(this.baseurl + '/prof/professionnels/',postContent, {headers:this.httpHeaders});
+    return this.http.post(this.baseurl + '/prof/professionnels/',postContent, {headers:this.httpHeaders,withCredentials: true});
   }
   postContact(tuteur,id_enfant): Observable<any>{
     let postContent={personne_id:-1,prenom:tuteur.prenom,nom:tuteur.nom,email:tuteur.email,telephone:tuteur.tel,relation:tuteur.statut,enfant:id_enfant};
-    return this.http.post(this.baseurl + '/prof/professionnels/',postContent, {headers:this.httpHeaders});
+    return this.http.post(this.baseurl + '/prof/professionnels/',postContent, {headers:this.httpHeaders,withCredentials: true});
   }
   delKid(id): Observable<any>{
-    return this.http.delete(this.baseurl + '/enfant/enfants/'+id+'/',{headers:this.httpHeaders});
+    return this.http.delete(this.baseurl + '/enfant/enfants/'+id+'/',{headers:this.httpHeaders,withCredentials: true});
   }
   connectUser(user): Observable<any>{
     let postContent=JSON.stringify(user);//{age:kid.age ,enfant_id:5,handicap:1,/*handicaps:kid.handicaps,*/nom:kid.nom,prenom:kid.prenom};
-    return this.http.post(this.baseurl + '/prof/login/',postContent, {headers:this.httpHeaders});
+    return this.http.post(this.baseurl + '/prof/login/',postContent, {headers:this.httpHeaders,withCredentials: true});
   }
   getFullSessionById(id): Observable<any>{
-    return this.http.get(this.baseurl + '/sessions/full_sessions/' + id + '/', {headers:this.httpHeaders});
+    return this.http.get(this.baseurl + '/sessions/full_sessions/' + id + '/', {headers:this.httpHeaders,withCredentials: true});
   }
   postNote(note): Observable<any>{
     let postContent = JSON.stringify(note);
-    return this.http.post(this.baseurl + '/sessions/notes/',postContent, {headers:this.httpHeaders});
+    return this.http.post(this.baseurl + '/sessions/notes/',postContent, {headers:this.httpHeaders,withCredentials: true});
   }
   postMandataire(mandataire): Observable<any>{
     let postContent = JSON.stringify(mandataire);
-    return this.http.post(this.baseurl + '/sessions/mandataires/',postContent, {headers:this.httpHeaders});
+    return this.http.post(this.baseurl + '/sessions/mandataires/',postContent, {headers:this.httpHeaders,withCredentials: true});
   }
 }
